53307ec1feb24f082f0c5d4ff7e695f52ee48009
detskiikrem <hellsangel@ya.ru>
2023-08-11 09:50:10 +0300

    Added script v 1.0

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import winreg
+
+def get_license_server_info():
+    # Путь к реестру для x86 и x64 систем
+    reg_paths = [
+        r"SOFTWARE\FLEXlm License Manager",
+        r"SOFTWARE\Wow6432Node\FLEXlm License Manager"
+    ]
+
+    license_file_found = False
+
+    for reg_path in reg_paths:
+        try:
+            # Открываем соответствующий путь реестра
+            with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path) as key:
+                # Получаем значение @CSOFT_LICENSE_FILE
+                try:
+                    license_file = winreg.QueryValueEx(key, "@CSOFT_LICENSE_FILE")[0]
+                    print("Путь к файлу лицензии:", license_file)
+                    license_file_found = True
+                except FileNotFoundError:
+                    pass
+        except FileNotFoundError:
+            pass
+
+    if not license_file_found:
+        print("Путь к серверу лицензий не найден.")
+
+if __name__ == "__main__":
+    get_license_server_info()
